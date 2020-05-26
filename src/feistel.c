1968649364e961f00124fd2166dee27b37d6ec57
NANISOREH <>
2020-05-26 11:40:28 +0200

    First single-block implementation

---
+++ src/feistel.c
@@ -0,0 +1,64 @@
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "utils.h"
+#define BLOCKSIZE 16
+#define KEYSIZE 8
+#define NROUND 10
+
+int feistel_round(unsigned char * left, unsigned char * right, unsigned char * key);
+void f(unsigned char * right, unsigned char * key);
+
+typedef struct block {
+    unsigned char left [BLOCKSIZE/2];
+    unsigned char right [BLOCKSIZE/2];
+    unsigned char round_key [KEYSIZE];
+}block; 
+
+int feistel(unsigned char * data, unsigned char * key) 
+{
+	block b;
+	for (int i=0; i<BLOCKSIZE/2; i++)
+	{
+		b.left[i] = data[i];
+		b.right[i] = data[i+8];
+	}
+	strncpy(b.round_key, key, KEYSIZE);
+
+	print_to_file(b.left, b.right);
+
+	for (int i=0; i<NROUND; i++)
+	{
+		feistel_round(b.left, b.right, b.round_key);
+	}
+	
+	//final inversion after the last round
+	unsigned char templeft[KEYSIZE];
+	strncpy(templeft, b.left, KEYSIZE);
+	strncpy(b.left, b.right, KEYSIZE);
+	strncpy(b.right, templeft, KEYSIZE);
+
+	print_to_file(b.left, b.right);
+}
+
+int feistel_round(unsigned char * left, unsigned char * right, unsigned char * key)
+{
+	unsigned char templeft[KEYSIZE];
+	strncpy(templeft, left, KEYSIZE);
+
+	strncpy(left, right, KEYSIZE);
+	f(right, key);
+	char_xor(right, templeft, right);
+}
+
+//placeholder substitution box
+void f(unsigned char * right, unsigned char * key)
+{
+	for (int i = 0; i<BLOCKSIZE/2; i++)
+	{
+		if (i % 2 == 0)
+			right[i] = right[i] + key[i];
+		else
+			right[i] = right[i] - key[i];
+	}
+}
---
+++ src/utils.h
@@ -0,0 +1,4 @@
+void print_byte(char c);
+int char_xor(unsigned char * result, unsigned char * first, unsigned char * second);
+void print_to_file(unsigned char * left, unsigned char * right);
+void read_from_file(unsigned char * buffer); 
