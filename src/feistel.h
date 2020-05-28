8e3a1bec6ec3d3d13036eadc430b63f15bb8c19d
NANISOREH <>
2020-05-28 13:27:47 +0200

    started to implement cbc

--- src/feistel.h
+++ src/feistel.h
@@ -1,6 +1,9 @@
 #define BLOCKSIZE 16
 #define KEYSIZE 8
 #define NROUND 10
+#define DEFAULT_MODE ecb
+
+enum mode{ecb, cbc_enc, cbc_dec}; 
 
 //this structure represents the state of a block throught the rounds
 typedef struct block {
@@ -10,6 +13,8 @@ typedef struct block {
 }block;
 
 void f(unsigned char * right, unsigned char * key); 
-void feistel_block(unsigned char * out, block b);
-unsigned char * feistel(unsigned char * data, unsigned char * key);
+void feistel_block(unsigned char * left, unsigned char * right, unsigned char * round_key);
+unsigned char * feistel(unsigned char * data, unsigned char * key, enum mode chosen);
+unsigned char * encrypt_cbc_mode(block * b, int bnum);
+unsigned char * decrypt_cbc_mode(block * b, int bnum);
 unsigned char * operate_ecb_mode(block * b, int bnum);
--- src/utils.c
+++ src/utils.c
@@ -5,13 +5,7 @@
 
 int char_xor(unsigned char * result, unsigned char * first, unsigned char * second)
 {
-	//different size for the two operands of a xor: should never happen in a feistel cipher
-	if (sizeof(first) != sizeof(second))
-	{
-		return -1;
-	}
-
-	for (int i = 0; i<sizeof(first); i++)
+	for (int i = 0; i<BLOCKSIZE/2; i++)
 	{
 		result[i] = first[i] ^ second[i];
 	}
